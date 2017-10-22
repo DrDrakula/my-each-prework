84192ef6ddfe2c4e4a1e8f5aed15b4fb8fc956f4
Nemanja Drakulovic <drdrakula96@hotmail.com>
2017-10-22 19:19:15 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  if block_given?
+    i = 0
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+    array
+  else
+    return "No black was given!"
+  end
+end
+
+array = ["Hi","my","name","is Nemanja"]
+
+my_each(array) do |word|
+
 end
